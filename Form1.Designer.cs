7ba1273e18ddcebc50750b943d481576093fa50b
bax082024 <bax082024@gmail.com>
2024-12-10 01:53:52 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GUI_API_Formss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGetMessage = new Button();
+            lblMessage = new Label();
+            SuspendLayout();
+            // 
+            // btnGetMessage
+            // 
+            btnGetMessage.Location = new Point(310, 169);
+            btnGetMessage.Name = "btnGetMessage";
+            btnGetMessage.Size = new Size(84, 23);
+            btnGetMessage.TabIndex = 0;
+            btnGetMessage.Text = "Get Message";
+            btnGetMessage.UseVisualStyleBackColor = true;
+            btnGetMessage.Click += btnGetMessage_Click;
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.Location = new Point(319, 102);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(0, 15);
+            lblMessage.TabIndex = 1;
+            
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblMessage);
+            Controls.Add(btnGetMessage);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnGetMessage;
+        private Label lblMessage;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace GUI_API_Formss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetMessage_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // API URL
+                    string apiUrl = "http://localhost:5000/api/test/message";
+
+                    // Send GET request
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    // Ensure success
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string message = await response.Content.ReadAsStringAsync();
+                        lblMessage.Text = message; // Display the message
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Error: " + response.StatusCode;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = "Exception: " + ex.Message;
+                }
+            }
+        }
+    }
+}
