f5bbeb12b4fb1d5af163b6b0221b905f16709e2a
bax082024 <bax082024@gmail.com>
2024-12-11 00:08:36 +0100

    added image

--- obj/Debug/net8.0-windows/GUI-API-Formss.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GUI-API-Formss.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI-API-Formss")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+135aba475d394fb83daae3c2a4aba1a609d68767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9fcd8b9abe8642a3018e50c70a043f6cf16be6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI-API-Formss")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI-API-Formss")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
