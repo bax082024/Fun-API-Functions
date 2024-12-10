0368267ecccf5997b543d4153ba6728762043af5
bax082024 <bax082024@gmail.com>
2024-12-10 23:25:21 +0100

    added .env

--- obj/Debug/net8.0-windows/GUI-API-Formss.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GUI-API-Formss.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI-API-Formss")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41d349c2126740c01fed75a58fd724e827ff8e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872c1aa91eca825110dc26b5fa7f38dd4a18b767")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI-API-Formss")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI-API-Formss")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
